5fa2957d76e984a31adb3478e0c902fb1ba5ad20
Carrie Guan <carrieguan17@gmail.com>
2020-11-17 17:43:26 -0800

    jest enzyme setup complete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Header from './components/Header.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +11,8 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <div>test</div>
+        <h1>test</h1>
+        <Header />
       </div>
     )
   }
