54dd7319464b0608752670ca2d7204d542bd644e
Carrie Guan <carrieguan17@gmail.com>
2020-11-20 22:25:57 -0800

    about section complete

--- client/src/components/Resources1.jsx
+++ client/src/components/Resources1.jsx
@@ -4,7 +4,51 @@ import styles from '../styles/Resources1.css';
 function Resources1 () {
   return(
     <div className={styles.resources1}>
-      <div>Resources1</div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>JavaScript</div>
+        <div className={styles.fillBar95}></div>
+        <div className={styles.numBar95}>95%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>React</div>
+        <div className={styles.fillBar95}></div>
+        <div className={styles.numBar95}>95%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>CSS</div>
+        <div className={styles.fillBar95}></div>
+        <div className={styles.numBar95}>95%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Axios/jQuery</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Webpack</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Figma</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>MongoDB</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>PostgreSQL</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>MySQL</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
     </div>
   )
 }
--- client/src/components/Resources2.jsx
+++ client/src/components/Resources2.jsx
@@ -4,7 +4,51 @@ import styles from '../styles/Resources2.css';
 function Resources2 () {
   return(
     <div className={styles.resources2}>
-      <div>Resources2</div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>NodeJS</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Docker</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>EC2</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>S3</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Nginx</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>New Relic</div>
+        <div className={styles.fillBar65}></div>
+        <div className={styles.numBar65}>65%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Mocha/Chai</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Jest/Enzyme</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.nameBar}>Loader.io</div>
+        <div className={styles.fillBar80}></div>
+        <div className={styles.numBar80}>80%</div>
+      </div>
     </div>
   )
 }
