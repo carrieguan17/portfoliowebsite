8b5040e75bdaab5098dd135feca2e7cb69bc188f
Carrie Guan <carrieguan17@gmail.com>
2020-11-25 22:00:07 -0800

    contact section complte

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -5,13 +5,15 @@ import classnames from 'classnames'
 function BtnIcon (props) {
   if (props.active !== props.title) {
     return(
-        <div>
+        <div onClick={() => props.setActive(props.title)}>
           <svg className={styles.icon} viewBox="-8 -10 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
         </div>
     )
   } else {
     return (
-      <div></div>
+      <div onClick={() => props.setActive("")}>
+        <svg className={styles.icon} viewBox="-10 -11 45 45" xmlns="http://www.w3.org/2000/svg"><path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/></svg>
+      </div>
     )
   }
 }
@@ -25,8 +27,8 @@ function Article (props) {
       <div className={styles.articleContainer}>
         <div className={styles.headingContainer}>
           <div className={styles.title}>{props.title}</div>
-          <div onClick={() => props.setActive(props.title)}>
-            <BtnIcon title={props.title} active={props.active}/>
+          <div>
+            <BtnIcon title={props.title} active={props.active} setActive={props.setActive}/>
           </div>
         </div>
         <div className={ classnames(styles.articleContent, props.active === props.title && styles.articleShow) }>
