9231ca98351ce229eca6d77e1e6569aecd561f9e
Carrie Guan <carrieguan17@gmail.com>
2021-02-20 17:46:20 -0800

    fixed spelling error

--- client/src/components/AboutWords.jsx
+++ client/src/components/AboutWords.jsx
@@ -17,7 +17,7 @@ function AboutWords () {
             I have been a full-stack software engineer since 2019 when I built a business website from scratch for a CPA firm. Currently I am working in a start-up <a href="https://leetcall.com" target="_blank" className={styles.introLink}>leetcall.com</a> for an automatic scheduling platform. I enjoy making good design and translate visual ideas into codes. It feels very rewarding to create new features and refine user experience. And most importantly, it is exciting to work with like-minded people and challenge each other's ideas in refining algorithm, optimizing website performance, and improving code cleanliness.
           </p>
           <p>
-            A few simple lines of codes can change our life or even restructure the system in certin industries. If you have a similar life pursuit, I look forward to connnecting with you.
+            A few simple lines of codes can change our life or even restructure the system in certin industries. If you have a similar life pursuit, I look forward to connecting with you.
           </p>
         </div>
     </div>
